3a601f04fe279887642653a647035e81f6228ef0
eeastman <elizabeth@bee-haven.com>
2018-04-09 16:27:31 -0400

    Create image_test_noPCA.py

---
+++ image_test_noPCA.py
@@ -0,0 +1,104 @@
+import os
+import numpy as np
+import tensorflow as tf
+
+from sklearn.decomposition import PCA
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+
+import vgg16
+import utils
+
+
+def getPoolingLayers(batch, batch_size):
+    layerPath = './pool_layers/' # CHANGE SO YOU DON'T OVERRIDE
+
+    # with tf.Session(config=tf.ConfigProto(gpu_options=(tf.GPUOptions(per_process_gpu_memory_fraction=0.7)))) as sess:
+    with tf.device('/cpu:0'):
+        with tf.Session() as sess:
+            images = tf.placeholder("float", [batch_size, 224, 224, 3])
+            feed_dict = {images: batch}
+
+            vgg = vgg16.Vgg16()
+            with tf.name_scope("content_vgg"):
+                vgg.build(images)
+
+            # pool1 = sess.run(vgg.pool1, feed_dict=feed_dict)
+            # np.save(os.path.join(layerPath + 'pool1.npy'),pool1)
+            
+            # pool2 = sess.run(vgg.pool2, feed_dict=feed_dict)
+            # np.save(os.path.join(layerPath + 'pool2.npy'),pool2)
+
+            # pool3 = sess.run(vgg.pool3, feed_dict=feed_dict)
+            # np.save(os.path.join(layerPath + 'pool3.npy'),pool3)
+
+            # pool4 = sess.run(vgg.pool4, feed_dict=feed_dict)
+            # np.save(os.path.join(layerPath + 'pool4.npy'),pool4)
+
+            pool5 = sess.run(vgg.pool5, feed_dict=feed_dict)
+            np.save(os.path.join(layerPath + 'pool5.npy'),pool5)
+
+            prob = sess.run(vgg.prob, feed_dict=feed_dict)
+            print(prob)
+
+            return pool5
+
+def pca(batch_size):
+    # For one pooling layer right now
+    #X = np.zeros((100, 5, 6, 7)) # 100 images suppose  # REPLACE WITH SAVED OUTPUT FROM LAYER
+    X = np.load('./pool_layers/pool5.npy')
+    X = X.reshape((batch_size, -1)) # REPLACE 100 W/ NUMBER OF IMAGES 
+
+    pca = PCA(200)
+    print(pca)
+    # pca.fit(X)
+    # print(X.shape)
+    X_pca = pca.fit_transform(X)
+
+    print(X.shape)
+    print(X_pca.shape)
+
+    # np.save('./pca_layers/pool5.npy', X_pca)
+
+    # return X_pca
+
+def run_svm(batch_size):
+    X_imgs = np.load('./pool_layers/pool5.npy')
+    X_imgs = X_imgs.reshape((batch_size, -1))
+    y_labs = np.array([1, 1, 0, 0]) # NEED LABELS
+    clf = svm.SVC(kernel='linear', C=1).fit(X_imgs, y_labs)
+    scores = cross_val_score(clf, X_imgs, y_labs, cv=2) # CHANGE WHEN MORE DATA
+    # TODO SAVE
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+def loadImages(directory):
+    img_paths = sorted(os.listdir(directory))
+    print(img_paths)
+    images = []
+    batch_size = 0
+
+    for path in img_paths:
+        batch_size += 1
+        img = utils.load_image(directory + path)
+        resize = img.reshape((1, 224, 224, 3))
+        images.append(resize)
+
+    batch = np.zeros((batch_size, 224, 224, 3))
+    for i, image in enumerate(images):
+        batch[i, :, :, :] = image
+
+    return batch, batch_size
+
+def main():
+    # make directory/list variable
+    # labels?
+
+    directory = './test_data/'
+    batch, batch_size = loadImages(directory)
+    x = getPoolingLayers(batch, batch_size)
+    #X_pca = pca(batch_size)
+    run_svm(batch_size)
+
+main()
+
+
